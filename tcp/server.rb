34398688ab859dc8860d898251496668ada1fb98
Leandro Proença <leandronsp@gmail.com>
2022-01-13 00:45:40 +0000

    HTTP 101

--- tcp/server.rb
+++ tcp/server.rb
@@ -21,25 +21,3 @@ client.close
 
 # Close the socket
 socket.close
-
-socket = TCPServer.new(3000)
-puts 'Server created. Waiting for connections...'
-
-# Wait
-client = socket.accept
-
-puts 'Connection arrived'
-
-# Read from socket
-request = client.gets
-
-puts "Request ==> #{request}"
-
-# Write on socket
-client.puts('Yo, client!')
-
-# Close the client connection
-client.close
-
-# Close the socket
-socket.close
