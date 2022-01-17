c9273cd4b719bab4ce1c4ffc0b4d2bc7c3a551a1
leandronsp <leandronsp@gmail.com>
2022-01-17 21:19:44 +0000

    Using HTTP cookies to implement a persistent counter

--- http/server.rb
+++ http/server.rb
@@ -6,9 +6,35 @@ puts 'Listening to the port 3000...'
 loop do
   client = socket.accept
 
-  request = client.gets
+  request = ''
 
-  client.puts("HTTP/1.1 200\r\n\r\n\r\nHello!")
+  while line = client.gets
+    break if line == "\r\n"
+
+    request +=  line
+  end
+
+  puts request
+  puts "\n\n"
+
+  cookie = {}
+
+  if cookie_match = request.match(/Cookie:\s(.*)=(.*)\r$/)
+    cookie[cookie_match[1]] = cookie_match[2]
+  end
+
+  counter = cookie.fetch('counter', 0).to_i + 1
+
+  response =
+"""
+HTTP/1.1 200\r\n
+Content-Type: text/html\r\n
+Set-Cookie: counter=#{counter}; path=/; HttpOnly\r\n
+\r\n
+<h1>Counter: #{counter}</h1>
+"""
+
+  client.puts(response.strip.gsub(/\n+/, "\n"))
 
   client.close
 end
